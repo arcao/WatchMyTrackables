922d980e94eb5be394e6c7180c40cf10a9085f45
Arcao <arcao@arcao.com>
2014-04-21 19:52:42 +0200

    Added Dagger modules for services and data

---
+++ src/main/java/com/arcao/wmt/data/services/ServiceModule.java
@@ -0,0 +1,54 @@
+package com.arcao.wmt.data.services;
+
+import android.app.Application;
+
+import com.arcao.geocaching.api.GeocachingApi;
+import com.arcao.geocaching.api.configuration.GeocachingApiConfiguration;
+import com.arcao.geocaching.api.configuration.OAuthGeocachingApiConfiguration;
+import com.arcao.geocaching.api.configuration.impl.ProductionGeocachingApiConfigurationImpl;
+import com.arcao.geocaching.api.configuration.impl.StagingGeocachingApiConfigurationImpl;
+import com.arcao.geocaching.api.data.SimpleGeocache;
+import com.arcao.geocaching.api.impl.LiveGeocachingApi;
+import com.arcao.utils.cache.Cache;
+import com.arcao.wmt.BuildConfig;
+import com.arcao.wmt.data.services.geocache.GeocacheFileCache;
+import com.arcao.wmt.data.services.geocache.GeocacheService;
+
+import javax.inject.Singleton;
+
+import dagger.Module;
+import dagger.Provides;
+
+@Module(
+				complete = false,
+				library = true
+)
+public class ServiceModule {
+	@Provides
+	@Singleton
+	OAuthGeocachingApiConfiguration provideGeocachingApiConfiguration() {
+		if (BuildConfig.GEOCACHING_API_STAGING) {
+			return new StagingGeocachingApiConfigurationImpl();
+		} else {
+			return new ProductionGeocachingApiConfigurationImpl();
+		}
+	}
+
+	@Provides
+	@Singleton
+	GeocachingApi provideGeocachingApi(GeocachingApiConfiguration configuration) {
+		return new LiveGeocachingApi(configuration);
+	}
+
+	@Provides
+	@Singleton
+	Cache<String, SimpleGeocache> provideSimpleGeocacheCache(Application app) {
+		return new GeocacheFileCache(app);
+	}
+
+	@Provides
+	@Singleton
+	GeocacheService provideGeocacheService(Application app, GeocachingApi geocachingApi, Cache<String, SimpleGeocache> cache) {
+		return new GeocacheService.GeocacheServiceBuilder(app, geocachingApi).setCache(cache).build();
+	}
+}
