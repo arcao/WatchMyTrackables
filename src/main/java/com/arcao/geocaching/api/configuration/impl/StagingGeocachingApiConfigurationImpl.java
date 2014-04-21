ca74ceda37b1acf0f2ca9df7c74daf8af6156afe
Arcao <arcao@arcao.com>
2014-04-21 19:47:45 +0200

    Added configuration classes for Geocaching API

---
+++ src/main/java/com/arcao/geocaching/api/configuration/impl/StagingGeocachingApiConfigurationImpl.java
@@ -0,0 +1,31 @@
+package com.arcao.geocaching.api.configuration.impl;
+
+import com.arcao.geocaching.api.configuration.OAuthGeocachingApiConfiguration;
+import com.arcao.wmt.BuildConfig;
+
+public class StagingGeocachingApiConfigurationImpl extends DefaultStagingGeocachingApiConfiguration implements OAuthGeocachingApiConfiguration {
+	private static final String OAUTH_URL = "https://staging.geocaching.com/oauth/mobileoauth.ashx";
+
+	private static final String CONSUMER_KEY = BuildConfig.GEOCACHING_API_KEY;
+	private static final String CONSUMER_SECRET = BuildConfig.GEOCACHING_API_SECRET;
+
+	public String getConsumerKey() {
+		return CONSUMER_KEY;
+	}
+
+	public String getConsumerSecret() {
+		return CONSUMER_SECRET;
+	}
+
+	public String getOAuthRequestUrl() {
+		return OAUTH_URL;
+	}
+
+	public String getOAuthAuthorizeUrl() {
+		return OAUTH_URL;
+	}
+
+	public String getOAuthAccessUrl() {
+		return OAUTH_URL;
+	}
+}
