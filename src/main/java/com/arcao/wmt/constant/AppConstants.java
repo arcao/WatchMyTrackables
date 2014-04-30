30117edf2fa775d8031caa02cefe6526e641dc97
Arcao <arcao@arcao.com>
2014-04-30 08:43:40 +0200

    Added constants

---
+++ src/main/java/com/arcao/wmt/constant/AppConstants.java
@@ -0,0 +1,15 @@
+package com.arcao.wmt.constant;
+
+/**
+ * Created by msloup on 30.4.2014.
+ */
+public interface AppConstants {
+	static final String GEOCACHING_WEBSITE_URL = "http://www.geocaching.com/";
+	static final String OAUTH_CALLBACK_URL = "callback://wmt.oauth/";
+
+	/** OAuth response parameter that contains error message - specific Groundspeak extension */
+	static final String OAUTH_ERROR_MESSAGE_PARAMETER = "oauth_error_message";
+
+
+
+}
---
+++ src/main/java/com/arcao/wmt/constant/PrefConstants.java
@@ -0,0 +1,9 @@
+package com.arcao.wmt.constant;
+
+/**
+ * Created by msloup on 30.4.2014.
+ */
+public interface PrefConstants {
+	static final String USERNAME = "USERNAME";
+	static final String ACCESS_TOKEN = "ACCESS_TOKEN";
+}
