238b795e53e64b9e262a7328e8bd925dba9b45aa
Arcao <arcao@arcao.com>
2014-05-04 01:03:59 +0200

    Updated MainActivity to show User's own and favorited trackables
    Tabs in ActionBar is always embeded

--- src/main/java/com/arcao/wmt/ui/MainActivity.java
+++ src/main/java/com/arcao/wmt/ui/MainActivity.java
@@ -1,35 +1,36 @@
 package com.arcao.wmt.ui;
 
+import android.content.Context;
 import android.content.Intent;
-import android.os.AsyncTask;
+import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.app.ListFragment;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 
-import com.activeandroid.ActiveAndroid;
-import com.activeandroid.content.ContentProvider;
-import com.activeandroid.query.Delete;
-import com.arcao.geocaching.api.GeocachingApi;
-import com.arcao.geocaching.api.data.Trackable;
-import com.arcao.geocaching.api.exception.GeocachingApiException;
+import com.activeandroid.Model;
 import com.arcao.wmt.App;
 import com.arcao.wmt.R;
+import com.arcao.wmt.data.database.model.FavoritedTrackableModel;
 import com.arcao.wmt.data.database.model.MyTrackableModel;
 import com.arcao.wmt.data.services.account.AccountService;
 import com.arcao.wmt.ui.fragment.TrackableListFragment;
+import com.arcao.wmt.ui.task.UpdateMyTrackablesTask;
 
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.inject.Inject;
 import javax.inject.Provider;
 
-import timber.log.Timber;
-
 public class MainActivity extends ActionBarActivity {
 	@Inject
-	Provider<UpdateTask> updateTask;
+	Provider<UpdateMyTrackablesTask> updateTask;
 	@Inject
 	AccountService accountService;
 
@@ -40,16 +41,33 @@ public class MainActivity extends ActionBarActivity {
 		App.get(this).inject(this);
 		setContentView(R.layout.activity_main);
 
+		ActionBar actionBar = getSupportActionBar();
+		actionBar.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+		actionBar.setDisplayShowTitleEnabled(false);
+		actionBar.setDisplayShowHomeEnabled(false);
+
+		boolean landscape = getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE;
+		boolean showTabTitle = landscape || getResources().getBoolean(R.bool.actionbar_tab_title_show_always);
+
+		ActionBar.Tab tab = actionBar.newTab()
+						.setText(showTabTitle ? R.string.tab_my : R.string.tab_empty)
+						.setContentDescription(R.string.tab_my)
+						.setIcon(R.drawable.ic_action_social_person)
+						.setTabListener(new TabListener<>(MyTrackableModel.class));
+		actionBar.addTab(tab);
+		actionBar.selectTab(tab);
+
+		tab = actionBar.newTab()
+						.setText(showTabTitle ? R.string.tab_favorited : R.string.tab_empty)
+						.setIcon(R.drawable.ic_action_rating_favorite)
+						.setContentDescription(R.string.tab_favorited)
+						.setTabListener(new TabListener<>(FavoritedTrackableModel.class));
+		actionBar.addTab(tab);
+
 		if (!accountService.hasAccount()) {
 			startActivityForResult(new Intent(this, WelcomeActivity.class), 0);
 		}
-	}
-
-	@Override
-	protected void onResumeFragments() {
-		super.onResumeFragments();
-
-		getSupportFragmentManager().beginTransaction().replace(R.id.content, TrackableListFragment.newInstance(MyTrackableModel.class)).commit();
 	}
 
 	@Override
@@ -71,49 +89,74 @@ public class MainActivity extends ActionBarActivity {
 		}
 	}
 
-	protected static class UpdateTask extends AsyncTask<Void, Void, Void> {
-		private GeocachingApi api;
-		private App app;
-
-		@Inject
-		public UpdateTask(App app, GeocachingApi api) {
-			this.app = app;
-			this.api = api;
-		}
-
-		@Override
-		protected Void doInBackground(Void... params) {
-			try {
-				new Delete().from(MyTrackableModel.class).execute();
-				app.getContentResolver().notifyChange(ContentProvider.createUri(MyTrackableModel.class, null), null);
-
-				List<Trackable> trackables = api.getUsersTrackables(0, 30, 0, false);
-				ActiveAndroid.beginTransaction();
-				try {
-					for (Trackable trackable : trackables) {
-						new MyTrackableModel(trackable).save();
-					}
-					ActiveAndroid.setTransactionSuccessful();
-				}
-				finally {
-					ActiveAndroid.endTransaction();
-				}
-
-				app.getContentResolver().notifyChange(ContentProvider.createUri(MyTrackableModel.class, null), null);
-			} catch (GeocachingApiException e) {
-				Timber.e(e, e.getMessage());
-			}
-
-			return null;
-		}
-	}
-
 	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 		super.onActivityResult(requestCode, resultCode, data);
 
 		if (resultCode == RESULT_CANCELED) {
 			finish();
+		}
+	}
+
+	private Resources mResourcesImpl;
+
+	@Override
+	public Resources getResources() {
+		if (mResourcesImpl == null) {
+			mResourcesImpl = new ResourcesImpl(this, super.getResources());
+		}
+		return mResourcesImpl;
+	}
+
+	private static class ResourcesImpl extends Resources {
+		private Resources mResources;
+		private Set<Integer> mActionBarEmbedTabsIds = new HashSet<>();
+
+		ResourcesImpl(Context context, Resources resources) {
+			super(resources.getAssets(), resources.getDisplayMetrics(), resources.getConfiguration());
+
+			mResources = resources;
+
+			String packageName = context.getPackageName();
+			mActionBarEmbedTabsIds.add(mResources.getIdentifier("abc_action_bar_embed_tabs", "bool", packageName));
+			mActionBarEmbedTabsIds.add(mResources.getIdentifier("abc_action_bar_embed_tabs_pre_jb", "bool", packageName));
+			mActionBarEmbedTabsIds.add(mResources.getIdentifier("action_bar_embed_tabs", "bool", "android"));
+			mActionBarEmbedTabsIds.add(mResources.getIdentifier("action_bar_embed_tabs_pre_jb", "bool", "android"));
+			mActionBarEmbedTabsIds.remove(0);
+		}
+
+		@Override
+		public boolean getBoolean(int id) throws NotFoundException {
+			return mActionBarEmbedTabsIds.contains(id) || super.getBoolean(id);
+		}
+	}
+
+	private static class TabListener<M extends Model> implements ActionBar.TabListener {
+		protected final Class<M> model;
+		protected ListFragment listFragment;
+
+		public TabListener(Class<M> model) {
+			this.model = model;
+		}
+
+		@Override
+		public void onTabSelected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+			if (listFragment == null) {
+				listFragment = TrackableListFragment.newInstance(model);
+				fragmentTransaction.replace(R.id.content, listFragment, model.getSimpleName());
+			} else {
+				fragmentTransaction.attach(listFragment);
+			}
+		}
+
+		@Override
+		public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+			fragmentTransaction.detach(listFragment);
+		}
+
+		@Override
+		public void onTabReselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+
 		}
 	}
 }
