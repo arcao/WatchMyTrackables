8e04fee30b178b6864d29e9fd8ab497f8c08a70f
Arcao <arcao@arcao.com>
2014-05-04 00:46:14 +0200

    Hide dialog title

--- src/main/java/com/arcao/wmt/ui/fragment/dialog/OAuthLoginDialogFragment.java
+++ src/main/java/com/arcao/wmt/ui/fragment/dialog/OAuthLoginDialogFragment.java
@@ -9,6 +9,7 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -69,6 +70,7 @@ public class OAuthLoginDialogFragment extends AbstractDialogFragment implements 
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
+		setStyle(DialogFragment.STYLE_NO_TITLE, 0);
 		setRetainInstance(true);
 
 		// clear geocaching.com cookies
